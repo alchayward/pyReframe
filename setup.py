910d96d229176e91c4b350104fa30c926b6848ef
Andrew Hayward <alc.hayward@gmail.com>
2016-09-26 19:56:10 +1000

    fixed config stuff

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ setup(
     version='0.1',
     packages=['', ''],
     package_dir={'': 'pyReframe'},
+    download_url = 'https://github.com/alchayward/pyReframe/tarball/0.1'
     url='https://github.com/alchayward/pyReframe.git',
-    license='',
+    license='MIT',
     author='Andrew L. C. Hayward',
     author_email='alc.hayward@gmail.com',
     description='a port of clojures reframe to python', requires=['pyrsistent', 'rx'])
